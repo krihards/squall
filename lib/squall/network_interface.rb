b8e5511329c52812e53db849a24f8b0b11023909
Rihards Kopāns <krihards@gmail.com>
2014-07-28 15:23:48 +0300

    Add Network Interface to VS

--- lib/squall/network_interface.rb
+++ lib/squall/network_interface.rb
@@ -8,5 +8,28 @@ module Squall
       response = request(:get, "/virtual_machines/#{vm_id}/network_interfaces.json ")
       response.collect { |i| i['network_interface'] }
     end
+    
+    # Public: Add Network Interface to VS
+    # vm_id - ID of the virtual machine
+    #
+    # options - Params for adding the network interface to virtual machine:
+    #           label                           - Label of a network
+    #           rate_limit                      - Port speed of a network interface you wish to attach
+    #           network_join_id                 - ID of a physical network used to attach this network interface
+    #           primary                         - 1 if the interface is primary. Otherwise false.
+    #
+    # Example
+    #     create(
+    #       label:             'lan',
+    #       rate_limit:        100,
+    #       network_join_id:   2,
+    #       primary:           0
+    #     )
+    #
+    # Returns a Hash.
+    def create(vm_id, options = {})
+      response = request(:post, "/virtual_machines/#{vm_id}/network_interfaces.json", default_params(options))
+      response['virtual_machine']
+    end
   end
 end
