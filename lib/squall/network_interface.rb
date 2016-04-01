da11d00a555fb90ba49d256a6fb67ffe2861385d
Rihards <krihards@me.com>
2016-04-01 12:17:21 +0300

    bad URI fix

--- lib/squall/network_interface.rb
+++ lib/squall/network_interface.rb
@@ -5,7 +5,7 @@ module Squall
     #
     # Returns an Array.
     def list(vm_id)
-      response = request(:get, "/virtual_machines/#{vm_id}/network_interfaces.json ")
+      response = request(:get, "/virtual_machines/#{vm_id}/network_interfaces.json")
       response.collect { |i| i['network_interface'] }
     end
     
